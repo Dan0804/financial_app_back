064e43d258c6a4015f336d5d8cb55db12622cef6
Dan0804 <wheogus185@naver.com>
2024-12-08 23:01:40 +0900

    create user_table

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 
 base = declarative_base()
 
-class Test(base):
-    __tablename__ = "test"
+class Users(base):
+    __tablename__ = "user_table"
 
-    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
-    name = Column(String(255), nullable=False)
-    email = Column(String(255), nullable=False)
+    _id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    email = Column(String(40), nullable=False)
+    password = Column(String(255), nullable=False)
+    created_date = Column(Date, nullable=False)
+    auth = Column(String(8), nullable=False, default="customer")
